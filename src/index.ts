0da94aa8bc0a71cb77a9618cce977ab96bf7da51
Taylor Thompson <taylor@nightshiftwaxco.com>
2023-03-29 14:11:11 -0400

    Working version 1.0, needs to be cleaned up. Also ignoring text files now.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-const args = process.argv[2]
+const args: string[] = process.argv;
 
-const file = fs.readFileSync(`${args}/scratch.txt`, 'utf8');
+const file: string = fs.readFileSync(`${args[2]}`, 'utf8');
 
-console.log(file, args)
+const arr: string[] = file.trim().split('\n');
+
+function insertCustomText(arr: string[], textToAdd: string) {
+  let indexWeNeed;
+  for (let i = 0; i < arr.length; i++) {
+    const e = arr[i];
+    if (e === '}') {
+      console.log('found it', e, i);
+      indexWeNeed = i;
+      arr.splice(indexWeNeed, 0, textToAdd);
+      const newArr = arr.join('\n')
+      fs.writeFileSync('./test-final-write.txt', newArr)
+      console.log(arr);
+      return indexWeNeed;
+    }
+  }
+
+  //we need to insert above line 29
+  // console.log(arr[29], textToAdd);
+  return 'TODO';
+}
+insertCustomText(arr, '    text-color: @normal-foreground;');
+
+// console.log(file.trim())
